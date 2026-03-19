6e7a219a3c27fd84103337394cbb42234ab82fa2
Sakthi <mailtosakthi6@gmail.com>
2026-03-19 22:40:00 +0530

    leaning

--- src/BrowserRouter/App1.jsx
+++ src/BrowserRouter/App1.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 
